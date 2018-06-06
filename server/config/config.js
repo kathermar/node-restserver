0d427e9cd2f24805df559bcd5c3f299b4ad1cfc9
katherine <kathermar25@gmail.com>
2018-06-06 14:49:01 -0400

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,9 @@ if (process.env.NODE_ENV === 'dev') {
 
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:k25101991@ds251210.mlab.com:51210/cafe';
+    // creamos en heroku una variable de entorno a la que le asignamos el URL en dond se guarda el usuario 
+    // y contrase;a de la base de datos
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
